195a22689fb5ea3a191de16bb0f692b371839c1a
Sharath George M <sharathgeorgem@gmail.com>
2018-09-15 08:09:33 +0530

    Updated LISPInterpreter.js

--- LISPinterpreter.js
+++ LISPinterpreter.js
@@ -1,6 +1,15 @@
 // Parse and Evaluate function
 function parseAndEval (data, context) {
   if (context === undefined) return parseAndEval(data, new Context(environment))
+  // Check for begin
+  if (data.startsWith('(begin')) {
+    data = data.slice(7)
+    let definition = data.substr(0, data.indexOf(')') + 1)
+    parseAndEval(definition)
+    data = data.slice(definition.length)
+    data = clearSpace(data)
+    return parseAndEval(data)
+  }
   // Check for quote
   if (data.startsWith('\'')) return clearSpace(data.slice(1))
   if (data.startsWith('(quote')) return clearSpace(data.slice(6))
@@ -33,7 +42,7 @@ function parseAndEval (data, context) {
   if (data.startsWith('define')) {
     data = data.slice(6)
     data = clearSpace(data)
-    let key = data.substring(0, data.indexOf(' '))
+    var key = data.substring(0, data.indexOf(' '))
     data = clearSpace(data.slice(key.length))
     // Check for lambda
     if (data.startsWith('(lambda')) {
@@ -46,15 +55,34 @@ function parseAndEval (data, context) {
       console.log(localScope)
       data = data.slice(data.indexOf(' '))
       data = clearSpace(data)
+      data = data.slice(0, -2)
+      console.log(data)
+      let value = function (localScope, scope) {
+        return parseAndEval(String(data), new Context(localScope, scope))
+      }
+      context.scope[key] = value
+      console.log(context)
+      return ''
     }
     let value = data.substring(0, data.lastIndexOf(')'))
     data = clearSpace(data.slice(value.length))
-    environment[key] = value
+    context.scope[key] = value
     return ''
+  }
+  // Check for set
+  if (data.startsWith('set!')) {
+    data = data.slice(4)
+    data = clearSpace(data)
+    let symbol = data.substr(0, data.indexOf(' '))
+    data = data.slice(symbol.length)
+    data = clearSpace(data)
+    let value = data.substr(0, data.indexOf(')'))
+    if (context.get(symbol)) context.get[symbol] = value
+    else return 'Symbol does not exist'
   }
   let check = data.substr(0, data.indexOf(' '))
   data = data.slice(check.length)
-  if (environment.hasOwnProperty(check)) {
+  if (context.get(check)) {
     let arr = []
     while (!data.startsWith(')')) {
       data = clearSpace(data)
@@ -69,18 +97,15 @@ function parseAndEval (data, context) {
       if (/^[A-Za-z]+/.exec(data)) {
         let varName = /^[A-Za-z]+/.exec(data)[0]
         // Check with context
-        // if (varName in context.scope) replaceName = context.get(varName)
-        // else if (varName in context.parent) replaceName = context.get(varName)
         let replaceName = context.get(varName)
         data = data.replace(varName, replaceName)
-        console.log(data)
       }
       data = clearSpace(data)
       data = parseNumber(data)
       arr.push(data[0])
       data = clearSpace(data[1])
     }
-    return environment[check](arr)
+    return environment[check](arr, context.scope)
   } return null
 }
 // Environment variable
@@ -121,12 +146,7 @@ var environment = {
     })
   },
   'pi': 3.14,
-  'circle-area': (parameter, context) => {
-    console.log(context)
-    var localScope = Object.create(null)
-    localScope['r'] = parameter
-    return parseAndEval('(* pi(* r r))', new Context(localScope, new Context(environment)))
-  }
+  'r': 12
 }
 // Context constructor
 var Context = function (scope, parent) {
@@ -152,4 +172,5 @@ function clearSpace (data) {
   if (first === -1) return ''
   return data.slice(first)
 }
-console.log(parseAndEval('(circle-area 10)'))
+console.log(parseAndEval('(set! r (* r r))'))
+
